cccdbcd36a8eea05bf0c841c55360aab7a92023d
Masterchef365 <duncan.freeman1@gmail.com>
2024-12-11 16:40:38 -0800

    oooohhhhh I'm interpreting

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,63 @@
+use std::io::Write;
+
 use eframe::{
-    egui::{CentralPanel, ScrollArea, SidePanel, TextEdit, TopBottomPanel, Ui, Vec2},
+    egui::{
+        CentralPanel, Color32, RichText, ScrollArea, SidePanel, TextEdit, TopBottomPanel, Ui, Vec2,
+    },
     NativeOptions,
 };
 use egui_extras::syntax_highlighting::{highlight, CodeTheme};
+use rustpython_vm::compiler::Mode;
+use rustpython_vm::Interpreter;
 
 fn main() {
     let mut code = String::new();
-    let mut lang = String::new();
 
     eframe::run_simple_native("datathing", NativeOptions::default(), move |ctx, _frame| {
-        let width = ctx.available_rect().width();
-        SidePanel::left("leeft")
-            .show(ctx, |ui| {
-                let lang = lang.clone();
-                let mut layouter = move |ui: &Ui, string: &str, wrap_width: f32| {
-                    let mut layout_job = highlight(
-                        ui.ctx(),
-                        ui.style(),
-                        &CodeTheme::from_style(ui.style()),
-                        string,
-                        &lang
-                    );
+        SidePanel::left("leeft").show(ctx, |ui| {
+            let mut layouter = move |ui: &Ui, string: &str, wrap_width: f32| {
+                let mut layout_job = highlight(
+                    ui.ctx(),
+                    ui.style(),
+                    &CodeTheme::from_style(ui.style()),
+                    string,
+                    "py",
+                );
 
-                    layout_job.wrap.max_width = wrap_width;
-                    ui.fonts(|f| f.layout_job(layout_job.clone()))
-                };
+                layout_job.wrap.max_width = wrap_width;
+                ui.fonts(|f| f.layout_job(layout_job.clone()))
+            };
 
-                ScrollArea::vertical().show(ui, |ui| {
-                    ui.add(
-                        TextEdit::multiline(&mut code)
-                            .desired_width(f32::INFINITY)
-                            .desired_rows(50)
-                            .layouter(&mut layouter)
-                    );
-                });
+            ScrollArea::vertical().show(ui, |ui| {
+                ui.add(
+                    TextEdit::multiline(&mut code)
+                        .desired_width(f32::INFINITY)
+                        .desired_rows(50)
+                        .code_editor()
+                        .layouter(&mut layouter),
+                );
             });
+        });
 
         CentralPanel::default().show(ctx, |ui| {
-            ui.label("Language");
-            ui.text_edit_singleline(&mut lang);
+            Interpreter::without_stdlib(Default::default()).enter(|vm| {
+                let scope = vm.new_scope_with_builtins();
+                let code_obj = vm.compile(&code, Mode::Exec, "<embedded>".to_owned()); //.map_err(|err| vm.new_syntax_error(&err, Some(&code)));
+                match code_obj {
+                    Ok(obj) => {
+                        let clear_code = [0x1b, 0x5b, 0x48, 0x1b, 0x5b, 0x32, 0x4a, 0x1b, 0x5b, 0x33, 0x4a];
+                        let _ = std::io::stdout().write_all(&clear_code);
+                        let _ = std::io::stdout().flush();
+                        if let Err(_e) = vm.run_code_obj(obj, scope) {
+                            ui.label(RichText::new("Compile error").color(Color32::RED));
+                        } else {
+                        }
+                    }
+                    Err(e) => {
+                        ui.label(RichText::new(e.to_string()).color(Color32::RED));
+                    }
+                }
+            });
         });
     })
     .unwrap();
