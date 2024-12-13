8b7463cbcd75433d275e75bd97efb11c227a7095
Masterchef365 <duncan.freeman1@gmail.com>
2024-12-13 00:52:52 -0800

    Encapsulate

--- src/app.rs
+++ src/app.rs
@@ -2,7 +2,7 @@ use core::f32;
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use egui::{CentralPanel, Color32, RichText, ScrollArea, SidePanel, TextEdit, Ui};
+use egui::{CentralPanel, Color32, Id, Response, RichText, ScrollArea, SidePanel, TextEdit, Ui};
 use egui_extras::syntax_highlighting::{highlight, CodeTheme};
 //use egui_extras::syntax_highlighting::{highlight, CodeTheme};
 use rustpython_vm::Interpreter;
@@ -62,59 +62,10 @@ impl eframe::App for TemplateApp {
         let mut changed = false;
 
         SidePanel::left("leeft").show(ctx, |ui| {
-            let mut layouter = move |ui: &Ui, string: &str, wrap_width: f32| {
-                let mut layout_job = highlight(
-                    ui.ctx(),
-                    ui.style(),
-                    &CodeTheme::from_style(ui.style()),
-                    string,
-                    "py",
-                );
-
-                layout_job.wrap.max_width = wrap_width;
-                ui.fonts(|f| f.layout_job(layout_job.clone()))
-            };
-
             ScrollArea::vertical().show(ui, |ui| {
-                let ret = TextEdit::multiline(&mut self.code)
-                    .id("CodeEditor".into())
-                    .desired_width(f32::INFINITY)
-                    .desired_rows(50)
-                    .code_editor()
-                    .layouter(&mut layouter)
-                    .show(ui);
-
-                // Did we make a new line?
-                if ret.response.changed() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
-                    if let Some(cursor) = ret.cursor_range {
-                        let cursor = cursor.primary.ccursor;
-
-                        let prev_newline_idx = self.code[..cursor.index - 1].rfind('\n');
-
-                        if cursor.prefer_next_row {
-                            if let Some(prev) = prev_newline_idx {
-                                // Find the indent
-                                let indent_chars: String = self.code[prev..cursor.index]
-                                    .chars()
-                                    .take_while(|c| c.is_whitespace())
-                                    .filter(|c| *c == ' ' || *c == '\t')
-                                    .collect();
-
-                                // Insert indent
-                                self.code.insert_str(cursor.index, &indent_chars);
-
-                                // Set the new cursor pos
-                                let mut new_cursor_range = cursor;
-                                new_cursor_range.index += indent_chars.len();
-                                let mut new_state = ret.state;
-                                new_state.cursor.set_char_range(Some(egui::text::CCursorRange::one(new_cursor_range)));
-                                TextEdit::store_state(ui.ctx(), "CodeEditor".into(), new_state);
-                            }
-                        }
-                    }
-                }
-
-                changed |= ret.response.changed();
+                changed |=
+                    code_editor_with_autoindent(ui, "CodeEditor".into(), &mut self.code, "py")
+                        .changed();
             });
         });
 
@@ -174,4 +125,66 @@ impl eframe::App for TemplateApp {
                 });
         });
     }
+}
+
+fn code_editor_with_autoindent(
+    ui: &mut Ui,
+    id: Id,
+    code: &mut String,
+    lang: &'static str,
+) -> Response {
+    let mut layouter = move |ui: &Ui, string: &str, wrap_width: f32| {
+        let mut layout_job = highlight(
+            ui.ctx(),
+            ui.style(),
+            &CodeTheme::from_style(ui.style()),
+            string,
+            lang,
+        );
+
+        layout_job.wrap.max_width = wrap_width;
+        ui.fonts(|f| f.layout_job(layout_job.clone()))
+    };
+
+    let ret = TextEdit::multiline(code)
+        .id(id)
+        .desired_width(f32::INFINITY)
+        .desired_rows(50)
+        .code_editor()
+        .layouter(&mut layouter)
+        .show(ui);
+
+    // Did we make a new line?
+    if ret.response.changed() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+        if let Some(cursor) = ret.cursor_range {
+            let cursor = cursor.primary.ccursor;
+
+            let prev_newline_idx = code[..cursor.index - 1].rfind('\n');
+
+            if cursor.prefer_next_row {
+                if let Some(prev) = prev_newline_idx {
+                    // Find the indent
+                    let indent_chars: String = code[prev..cursor.index]
+                        .chars()
+                        .take_while(|c| c.is_whitespace())
+                        .filter(|c| *c == ' ' || *c == '\t')
+                        .collect();
+
+                    // Insert indent
+                    code.insert_str(cursor.index, &indent_chars);
+
+                    // Set the new cursor pos
+                    let mut new_cursor_range = cursor;
+                    new_cursor_range.index += indent_chars.len();
+                    let mut new_state = ret.state;
+                    new_state
+                        .cursor
+                        .set_char_range(Some(egui::text::CCursorRange::one(new_cursor_range)));
+                    TextEdit::store_state(ui.ctx(), id, new_state);
+                }
+            }
+        }
+    }
+
+    ret.response
 }
